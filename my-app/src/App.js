68ad1f712fd72cedae648ce395bbb04563fb9ca7
vutengklanricsi <richard.vuongtanquang@gmail.com>
2020-08-27 12:23:50 +0200

    12th Nesting components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Ninjas from './Ninjas'
 
 function App() {
   return (
     <div className="App">
      <h1>My first React app</h1>
      <p>Welcome</p>
+     <Ninjas />
     </div>
   );
 }
 
 export default App;
+
+// Az App componens a root komponens a reactben. a Root komponens az anya komponens ami főkomponens 
+// Ebbe az Root componensbe ágyazzuk be a többi komponenst 
