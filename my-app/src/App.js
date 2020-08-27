3db47bcbd996e8e1a2c4094841189432d06a6309
vutengklanricsi <richard.vuongtanquang@gmail.com>
2020-08-27 15:17:16 +0200

    13th-14th props, how to map over a props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Ninjas from './Ninjas'
 
-function App() {
-  return (
-    <div className="App">
-     <h1>My first React app</h1>
-     <p>Welcome</p>
-     <Ninjas name="Ryu" age="25" belt="black" />
-    </div>
-  );
+class App extends Component {
+    state = {
+      ninjas : [
+        { name: 'Ryu', age: 21, belt: 'black', id: 1},
+        { name: 'Yoshi', age: 23, belt: 'yellow', id: 2},
+        { name: 'Naruto', age: 21, belt: 'green', id: 3}
+      ]
+    }
+    render() {
+      return (
+      <div className="App">
+        <h1>My first React app</h1>
+        <p>Welcome</p>
+        <Ninjas ninjas = { this.state.ninjas } />
+      </div>
+    )
+  }
 }
 
 export default App;
-
 // Az App componens a root komponens a reactben. a Root komponens az anya komponens ami főkomponens 
 // Ebbe az Root componensbe ágyazzuk be a többi komponenst 
--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -2,13 +2,20 @@ import React, {Component} from 'react';
 
 class Ninjas extends Component{
   render() {
-    // console.log(this.props);
-    const { name, age, belt} = this.props
+    const { ninjas } = this.props;
+
+    const ninjaList = ninjas.map(ninja => { 
+      return (
+        <div className="ninja" key={ ninja.id }>
+          <div>Name: { ninja.name }</div>
+          <div>Age: { ninja.age }</div>
+          <div>Belt: { ninja.belt }</div>
+        </div>
+      )
+    })
     return (
-      <div className="ninja">
-        <div>Name: { this.props.name }</div>
-        <div>Age: { this.props.age }</div>
-        <div>Belt: { this.props.belt }</div>
+      <div className="ninjaList">
+        { ninjaList }
       </div>
     )
   }
