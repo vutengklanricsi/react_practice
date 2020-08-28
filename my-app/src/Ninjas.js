cef184f6d4e53b18f6449ee88ead4edd9a4bb9c7
vutengklanricsi <richard.vuongtanquang@gmail.com>
2020-08-28 12:04:07 +0200

    15th container components, UI components

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,24 +1,23 @@
 import React, {Component} from 'react';
 
-class Ninjas extends Component{
-  render() {
-    const { ninjas } = this.props;
-
-    const ninjaList = ninjas.map(ninja => { 
-      return (
-        <div className="ninja" key={ ninja.id }>
-          <div>Name: { ninja.name }</div>
-          <div>Age: { ninja.age }</div>
-          <div>Belt: { ninja.belt }</div>
-        </div>
-      )
-    })
+// 10-ik sorban adni kell neki egy key-t
+const Ninjas = ({ ninjas }) => {
+  const ninjaList = ninjas.map(ninja => { 
     return (
-      <div className="ninjaList">
-        { ninjaList }
+      <div className="ninja" key={ ninja.id }> 
+        <div>Name: { ninja.name }</div>
+        <div>Age: { ninja.age }</div>
+        <div>Belt: { ninja.belt }</div>
       </div>
     )
-  }
+  })
+  return (
+    <div className="ninjaList">
+      { ninjaList }
+    </div>
+  )
 }
 
 export default Ninjas;
+
+//classoknál automatikusan rákapcsolódik a props és nem kell paraméterként megadni. Viszont functionöknél átkell adni a props-ot
