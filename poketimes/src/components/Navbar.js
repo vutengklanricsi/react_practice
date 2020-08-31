b8d9be862b980517988b58335cb21d2a383fa2b9
vutengklanricsi <richard.vuongtanquang@gmail.com>
2020-08-31 20:06:57 +0200

    26th Using Link and Navlink instead of a tag

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link, NavLink} from 'react-router-dom'
 
 const Navbar = () =>  {
   return (
@@ -6,9 +7,9 @@ const Navbar = () =>  {
       <div className="container">
         <a className="brand-logo">Poke'times</a>
         <ul className="right">
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
         </ul>
       </div>
     </nav>
@@ -16,3 +17,7 @@ const Navbar = () =>  {
 }
 
 export default Navbar;
+// Link to - megakadályozza, hogy egy requestet küldjön a szerverbe
+// Navlink -el rárak egy active class-t az anchor tagre. A link taggel nem kapjuk meg ezt az active class-t
+// Navlink az akkor aktiválódik amikor rákattintunk a gombra ami átírányit a URL-be
+// Navlink ez akkor hasznos amikor más stílust akarunk rárakni az aktív link-ekre
