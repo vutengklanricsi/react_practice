ee391a12a6a756d1f31c26cc42b27e1324c9f887
vutengklanricsi <richard.vuongtanquang@gmail.com>
2020-09-02 15:16:16 +0200

    32th Using switch to avoid variable think load two components

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Switch } from 'react';
 import Navbar from './components/Navbar';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Home from './components/Home';
@@ -12,10 +12,12 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/:post_id" component={Post} /> 
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/:post_id" component={Post} />  
+          </Switch>
         </div>
       </BrowserRouter>
     );
@@ -30,3 +32,7 @@ export default App;
 //14-es sorban a az exact szóval tudjuk azt megcsinálni, hogy  a többi komponensek ne töltsenek be a homera
 // A 11-es sorban lévő browserrouter alá bármit hozzáadunk akkor hozzáadja azokat a propertieseket(Amikor propsokat adunk a komponensekhez)
 //Ebben az esetben a Navbar nincs benne, viszont adhatunk neki magasabb rendú komponenst
+
+//32th Switch tags - switch tag a 15-ik sorban azt csinálja, hogy az a route kapja az elsőbbséget amelyikre kattintottam és nem akarom, hogy két route legyen egy
+// /:post_id- az egy változó és ha rákattintok egy routera és aztán a másikra akkor a két komponens meg fog jelenni
+// végigiterál, hogy route komponenseken és megkeresi azt amelyiket beszeretnénk tölteni.
